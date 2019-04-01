83aaeaa91aba3b3f9c54aa6ecfe2e2ac0db344f7
Jaimal Dullat <jaimaldullat@gmail.com>
2019-04-01 13:50:32 -0400

    Update DB credentials

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -4,8 +4,8 @@
 // 1. Easy to exclude this file from source code managers
 // 2. Unique credentials on development and production server
 // 3. Unique credentials if working with multiple developers
-define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "chain_gang");
+define("DB_SERVER", "us-cdbr-iron-east-03.cleardb.net");
+define("DB_USER", "bb44875288343c");
+define("DB_PASS", "43c4f090");
+define("DB_NAME", "heroku_cad4bf8f08b92e6");
 ?>
